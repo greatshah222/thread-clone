fae2a9c4c8b2b3aef703bf6eea401e4b0512675f
Bishal Shah <great.shah222@gmail.com>
2023-08-13 01:40:12 +0300

    user card

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,57 @@
+import UserCard from "@/components/cards/UserCard";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+	const user = await currentUser();
+
+	if (!user) return null;
+
+	const userInfo = await fetchUser(user.id);
+
+	console.log("userInfo", userInfo);
+
+	if (!userInfo?.onboarded) redirect("/onboarding");
+
+	// FETCH USERS
+	const result = await fetchUsers({
+		userId: user.id,
+		searchString: "",
+		pageNumber: 1,
+		pageSize: 25,
+	});
+
+	return (
+		<section>
+			<h1 className="head-text mb-10">Search</h1>
+
+			{/* // SEARCH BAR */}
+
+			<div className="mt-14 flex flex-col gap-9">
+				{result.users.length === 0 ? (
+					<p className="no-result">No results</p>
+				) : (
+					<>
+						{result.users.map((el) => (
+							<UserCard
+								key={el?.id}
+								id={el.id}
+								name={el.name}
+								username={el?.username}
+								imgUrl={el.image}
+								personType="User"
+							/>
+						))}
+					</>
+				)}
+			</div>
+		</section>
+	);
+};
+
+export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,5 @@
 "use server";
+import { FilterQuery, SortOrder } from "mongoose";
 
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
@@ -85,3 +86,57 @@ export const fetchUserPosts = async (userId: string) => {
 		throw new Error(`Failed to fetch user posts: ${error?.message} `);
 	}
 };
+
+// Almost similar to Thead (search + pagination) and Community (search + pagination)
+export async function fetchUsers({
+	userId,
+	searchString = "",
+	pageNumber = 1,
+	pageSize = 20,
+	sortBy = "desc",
+}: {
+	userId: string;
+	searchString?: string;
+	pageNumber?: number;
+	pageSize?: number;
+	sortBy?: SortOrder;
+}) {
+	try {
+		connectToDB();
+
+		// Calculate the number of users to skip based on the page number and page size.
+		const skipAmount = (pageNumber - 1) * pageSize;
+
+		// Create a case-insensitive regular expression for the provided search string. i means case insenstive
+		const regex = new RegExp(searchString, "i");
+
+		// Create an initial query object to filter users.
+		const query: FilterQuery<typeof User> = {
+			// ne means not equal to current userid
+			id: { $ne: userId }, // Exclude the current user from the results.
+		};
+
+		// If the search string is not empty, add the $or operator to match either username or name fields.
+		if (searchString.trim() !== "") {
+			query.$or = [{ username: { $regex: regex } }, { name: { $regex: regex } }];
+		}
+
+		// Define the sort options for the fetched users based on createdAt field and provided sort order.
+		const sortOptions = { createdAt: sortBy };
+
+		const usersQuery = User.find(query).sort(sortOptions).skip(skipAmount).limit(pageSize);
+
+		// Count the total number of users that match the search criteria (without pagination).
+		const totalUsersCount = await User.countDocuments(query);
+
+		const users = await usersQuery.exec();
+
+		// Check if there are more users beyond the current page.
+		const isNext = totalUsersCount > skipAmount + users.length;
+
+		return { users, isNext };
+	} catch (error) {
+		console.error("Error fetching users:", error);
+		throw error;
+	}
+}
